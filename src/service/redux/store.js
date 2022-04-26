d1faf9d87997b70b4a4ce51bfc11f2e910d80dd5
Chris.c <cjs120dd@gmail.com>
2022-04-26 10:32:27 +0900

    간단한 redux-persist 구현 완료, 미들웨어를 사용한 제어는 추후 학습 예정

--- src/service/redux/store.js
+++ src/service/redux/store.js
@@ -1,10 +1,44 @@
 import {combineReducers, configureStore, } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from "redux-persist/lib/storage/session";
+
 import {userSlice} from "./user/slice";
-import {youtubeSlice} from "./yotubeData/slice";
+import { youtubeSlice} from "./yotubeData/slice";
+
+const persistConfig = {
+    key: "root",
+    storage,
+    version: 1,
+    whitelist: ["youTubeData"]
+}
 
 const rootReducer = combineReducers({
     user: userSlice.reducer,
     youTubeData: youtubeSlice.reducer,
 })
 
-export default configureStore({reducer: rootReducer});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware(
+            {
+                serializableCheck: {
+                    ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+                }
+            }
+        )
+});
+export let persistor = persistStore(store)
+
+export default store
